9073b475665714e1561ca65e86443f145d40e89c
datastory <honza@datastory.cz>
2021-02-24 09:06:50 +0100

    aktualizace dat

--- anal.py
+++ anal.py
@@ -1,9 +1,13 @@
 # %%
 import pandas as pd
+import numpy as np
 # %%
-d = pd.read_excel('./IS_COVID19_EPIDEMIOLOGIE_2021leden14.xlsx', sheet_name='celkovy prehled', skiprows=6)
-d65 = pd.read_excel('./IS_COVID19_EPIDEMIOLOGIE_2021leden14.xlsx', sheet_name='prehled vek 65+', skiprows=6)
-d75 = pd.read_excel('./IS_COVID19_EPIDEMIOLOGIE_2021leden14.xlsx', sheet_name='prehled vek 75+', skiprows=6)
+d = pd.read_excel('./04_IS_COVID19_EPIDEMIOLOGIE.xlsx', sheet_name='celkovy_prehled', skiprows=9)
+d65 = pd.read_excel('./04_IS_COVID19_EPIDEMIOLOGIE.xlsx', sheet_name='prehled_vek_65', skiprows=9)
+d75 = pd.read_excel('./04_IS_COVID19_EPIDEMIOLOGIE.xlsx', sheet_name='prehled_vek_75', skiprows=9)
+
+# %%
+d
 # %%
 d = d[['Datum', 'Aktuální počet hospitalizovaných osob']]
 d.columns = ['datum', 'hosp']
@@ -29,4 +33,11 @@ d['hosp_75_pct'] = d.hosp_75 / d.hosp
 d['hosp_mladsi_pct'] = d.hosp_mladsi / d.hosp
 # %%
 #d.to_excel('hosp_vek.xlsx', index=False)
+d.datum = pd.to_datetime(d.datum, format='%d. %m. %Y')
+# %%
+d['ts'] = d.datum.values.astype(np.int64) // 10 ** 9
+# %%
+d[['ts', 'hosp_mladsi', 'hosp_65', 'hosp_75']].to_csv('./dump.csv', encoding='utf-8', index=False, sep='\t')
+# %%
+d.columns
 # %%
